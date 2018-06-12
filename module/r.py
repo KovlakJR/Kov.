55c8328f5499d8aca0b5ecbf44417d257fff01c5
KovlakJR <40174525+KovlakJR@users.noreply.github.com>
2018-06-12 03:53:32 +0000

    Create r.py

---
+++ module/r.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+
+import time
+
+class bcolors:
+
+    HEADER = '\033[95m'
+
+    OKBLUE = '\033[94m'
+
+    OKGREEN = '\033[92m'
+
+    WARNING = '\033[93m'
+
+    FAIL = '\033[91m'
+
+    ENDC = '\033[0m'
+
+    BOLD = '\033[1m'
+
+print ("Luwak White Coffe ")
+
+print ("Passwordnya?")
+
+ 
+
+print"-<><><><><><><><><><><><><><>-"
+
+print"+1. Janda Semakin Didepan -"
+
+print"+ +"
+
+print"+2. Yamaha Semakin Didepan -"
+
+print"+ +"
+
+print"-<><><><><><><><><><><><><><>-"
+
+num1 = raw_input("pilih  Cuk : ")
+
+if num1 == "1":
+
+    print"Otak Vokep Njirr:) "
+
+elif num1 == "2":
+
+    print"Salah Bego:v"
+
+else:
+
+    print"kuy gelud tod:*"
+
+
+
+while True:
+
+ print bcolors.WARNING + ("Tobat Lah Nak Kiamat Udah dekat:)") + bcolors.ENDC
